e71cf7145ada2874d238f51c3d98a7a39afcfffa
Csorgo Balazs <balazs.csorgo01@gmail.com>
2023-02-09 14:56:46 +0100

    - Modified gitignore

--- src/main/java/org/example/Sex.java
+++ src/main/java/org/example/Sex.java
@@ -4,13 +4,30 @@ public enum Sex {
     M("M"),
     F("F");
 
-    private final String st;
+    private String st;
 
     Sex(String st) {
         this.st = st;
     }
 
     public String getSt() {
-        return this.st;
+        return st;
     }
+
+    public static Sex fromString(String st) {
+        for (Sex sex: Sex.values()) {
+                if (sex.st.equalsIgnoreCase(st)) {
+                return sex;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Sex{" +
+                "st='" + st + '\'' +
+                '}';
+    }
+
 }
